adc9905e09b9ccf96953859a5c29353e79ef8187
Abhishek <70491019+abhisheksai22@users.noreply.github.com>
2025-06-27 17:37:59 +0530

    Initial commit

---
+++ airtel-clone/src/main/java/com/abhi/airtel/AirtelCloneApplication.java
@@ -0,0 +1,13 @@
+package com.abhi.airtel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirtelCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirtelCloneApplication.class, args);
+	}
+
+}
